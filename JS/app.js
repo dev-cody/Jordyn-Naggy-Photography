5b63d65e2f1a161b80739222cbb671b9c14786b0
cody.koscielski <cody.koscielski@outlook.com>
2020-07-15 19:41:56 -0400

    Completed the flip cards and added more styles

--- JS/app.js
+++ JS/app.js
@@ -1,8 +1,9 @@
 
-
+//Grab the toggle button and navlinks
 const toggle = document.querySelector('.toggle');
 const navbarLinks = document.querySelector('.nav-links');
 
+//Click function to active
 toggle.addEventListener('click', () => {
     navbarLinks.classList.toggle('active');
 });
