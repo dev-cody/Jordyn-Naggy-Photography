9b479be2bc92f5d8428adc222985fefe6f3b35f5
cody.koscielski <cody.koscielski@outlook.com>
2020-07-26 19:31:31 -0400

    Created a slide show home page

--- JS/app.js
+++ JS/app.js
@@ -1,9 +1,24 @@
 
-//Grab the toggle button and navlinks
-const toggle = document.querySelector('.toggle');
-const navbarLinks = document.querySelector('.nav-links');
+//Slide Show
+let slideIndex = 0;
+slideshow();
 
-//Click function to active
-toggle.addEventListener('click', () => {
-    navbarLinks.classList.toggle('active');
-});
+function slideshow() {
+    //Get the elements from the DOM
+    let i;
+    let slides = document.querySelectorAll(".slides");
+
+    //loop through the slides
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    //Increment the slide index
+    slideIndex++;
+    if ( slideIndex > slides.length) {
+        slideIndex = 1;
+    }
+    //Display the images
+    slides[slideIndex - 1].style.display = "block";
+    //Set timer for the slides to show 3 secs
+    setTimeout(slideshow, 3000);// Changes images every three seconds
+}
